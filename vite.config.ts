ca6639f0c79f81b06194cb0b53094134291a85d9
Maria Moran <mariamoranluaces@gmail.com>
2025-01-27 20:05:24 +0100

    feat: config PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  workbox: {
+    maximumFileSizeToCacheInBytes: 3 * 1024 ** 2,
+  },
+  manifest: {
+    name: "Wishes",
+    short_name: "Wishes",
+    description: "An easy app to create and share wish lists",
+
+    icons: [
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/icon-192-maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
