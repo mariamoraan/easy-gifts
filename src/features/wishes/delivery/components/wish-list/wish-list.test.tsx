2a5a48be332a360916b5ece8c0c93e8233678363
Maria Moran <mariamoranluaces@gmail.com>
2025-02-12 19:06:39 +0100

    fix: remove unused import

--- src/features/wishes/delivery/components/wish-list/wish-list.test.tsx
+++ src/features/wishes/delivery/components/wish-list/wish-list.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect } from "vitest";
 import { WishList } from "./wish-list.component";
 import { MemoryRouter } from "react-router-dom";
 import { Wish } from "../../../domain/entities/wish.entity";
